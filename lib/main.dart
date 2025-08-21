dc92841b527075410d1342f8e66ea23394feda82
Electronica Care <mahamedmahmoud3880@gmail.com>
2025-08-21 22:01:27 +0300

    added splash screen light mood. init screenUtils. added assets to pubspec.yaml. init homepage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'UI/home-screen.dart';
+import 'UI/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child:MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: SplahScreen.routeName,
+        routes: {
+          SplahScreen.routeName:(context)=>SplahScreen(),
+          HomeScreen.routeName:(context)=>HomeScreen(),
+        },
+      ) ,
+    );
+  }
 
 }
